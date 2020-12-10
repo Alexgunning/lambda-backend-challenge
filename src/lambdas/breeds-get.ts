58f49189b4e5b8933fb05bc054a35c7b7dd52a52
Alex Gunning <agunning@coolfiresolutions.com>
2020-12-09 19:19:04 -0700

    yarn lint fixes

--- src/lambdas/breeds-get.ts
+++ src/lambdas/breeds-get.ts
@@ -15,52 +15,49 @@ interface Map {
   [key: string]: string[]
 }
 
-
 interface DogBreedList {
   message: Map
   status: string
 }
 
 export async function handler(): Promise<DogBreedListResponse | DogBreedListErrorResponse> {
-  let res:Response;
+  let res: Response
   try {
-    res = await fetch('https://dog.ceo/api/breeds/list/all');
-  }
-  catch(e) {
-    //Handle unreachable addresses
-    //Tested on https://frog.ceo/api/breeds/list/all 
+    res = await fetch('https://dog.ceo/api/breeds/list/all')
+  } catch (e) {
+    // Handle unreachable addresses
+    // Tested on https://frog.ceo/api/breeds/list/all
     if (e instanceof FetchError) {
-     return{ 
-       statusCode: 500,
-       errorMessage: e.message
-     }
-    }
-    else{
-      //I don't know what other errors node-fetch would return and I have never used the package before
       return {
-      statusCode: 500,
-      errorMessage: 'unknown error'
+        statusCode: 500,
+        errorMessage: e.message,
       }
+    }
+
+    // I don't know what other errors node-fetch would return and I have never used the package before
+    return {
+      statusCode: 500,
+      errorMessage: 'unknown error',
     }
   }
 
-  if(res.status != 200) {
+  if (res.status !== 200) {
     return {
       statusCode: res.status,
-      errorMessage: res.statusText
+      errorMessage: res.statusText,
     }
   }
   const payload: DogBreedList = await res.json()
-  let flattendDogBreeds:string[] = [];
-  for (let breed of Object.keys(payload.message)) {
-    let subBreed = payload.message[breed];
-    if (subBreed.length == 0) {
-      flattendDogBreeds.push(breed);
+  const dogMappings = payload.message
+
+  const dogBreeds = Object.keys(dogMappings).map(breed => {
+    if (dogMappings[breed].length === 0) {
+      return [breed]
     }
-    else{
-      flattendDogBreeds = flattendDogBreeds.concat(subBreed);
-    }
-  }
+    return dogMappings[breed]
+  })
+  //flatten array of arrays
+  const flattendDogBreeds = Array.prototype.concat.apply([], dogBreeds)
 
   return {
     statusCode: 200,
--- src/lambdas/types.ts
+++ src/lambdas/types.ts
@@ -1,6 +1,6 @@
 // types specific to function handlers
 
-//I changed the name because it was conflicting with the response type of node fetch 
+// I changed the name because it was conflicting with the response type of node fetch
 export interface InternalResponse {
   statusCode: number
 }
